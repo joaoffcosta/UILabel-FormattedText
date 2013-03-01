725b2b040b1e3f99c6dde4857715d4e8640fa801
joao.costa@hole19golf.com <joao.costa@hole19golf.com>
2013-03-01 12:28:27 +0000

    Initial Commit

---
+++ UILabel+FormattedText/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  UILabel+FormattedText
+//
+//  Created by Joao Costa on 3/1/13.
+//  Copyright (c) 2013 none. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ UILabel+FormattedText/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UILabel+FormattedText
+//
+//  Created by Joao Costa on 3/1/13.
+//  Copyright (c) 2013 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
