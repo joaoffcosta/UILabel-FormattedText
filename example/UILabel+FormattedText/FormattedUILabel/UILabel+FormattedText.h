c50a0591c44f1e31cdeee0e31616e957e7881372
joao.costa@hole19golf.com <joao.costa@hole19golf.com>
2013-03-01 20:09:33 +0000

    Moves example to a separate page and adds the UILabel+FormattedText class extension to the main folder

---
+++ example/UILabel+FormattedText/FormattedUILabel/UILabel+FormattedText.h
@@ -0,0 +1,16 @@
+//
+//  UILabel+FormattedText.h
+//  UILabel+FormattedText
+//
+//  Created by Joao Costa on 3/1/13.
+//  Copyright (c) 2013 none. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (FormattedText)
+
+- (void)setTextColor:(UIColor *)textColor range:(NSRange)range;
+- (void)setFont:(UIFont *)font range:(NSRange)range;
+
+@end
---
+++ example/UILabel+FormattedText/FormattedUILabel/UILabel+FormattedText.m
@@ -0,0 +1,33 @@
+//
+//  UILabel+FormattedText.m
+//  UILabel+FormattedText
+//
+//  Created by Joao Costa on 3/1/13.
+//  Copyright (c) 2013 none. All rights reserved.
+//
+
+#import "UILabel+FormattedText.h"
+
+@implementation UILabel (FormattedText)
+
+- (void)setTextColor:(UIColor *)textColor range:(NSRange)range
+{
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithAttributedString: self.attributedText];
+    [text addAttribute: NSForegroundColorAttributeName
+                 value: textColor
+                 range: range];
+    
+    [self setAttributedText: text];
+}
+
+- (void)setFont:(UIFont *)font range:(NSRange)range
+{
+    NSMutableAttributedString *text = [[NSMutableAttributedString alloc] initWithAttributedString: self.attributedText];
+    [text addAttribute: NSFontAttributeName
+                 value: font
+                 range: range];
+    
+    [self setAttributedText: text];
+}
+
+@end
