ce086b90bd5a4896caf9568f8d600df450fb6bb6
joao.costa@hole19golf.com <joao.costa@hole19golf.com>
2013-03-01 12:33:56 +0000

    Writes use case for UILabel+FormattedText

--- UILabel+FormattedText/ViewController.m
+++ UILabel+FormattedText/ViewController.m
@@ -18,6 +18,41 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+
+    // Colors
+    UILabel *labelC = [[UILabel alloc] init];
+    [labelC setFrame:CGRectMake(0, 0, self.view.frame.size.width, floorf(self.view.frame.size.height / 3))];
+    [labelC setBackgroundColor:[UIColor clearColor]];
+    [labelC setTextAlignment:NSTextAlignmentCenter];
+    [labelC setText:@"This is a text with\nRED and CYAN"];
+    [labelC setNumberOfLines:2];
+    [labelC setTextColor:[UIColor yellowColor]];
+    [labelC setFont:[UIFont fontWithName:@"Courier" size:14]];
+    [self.view addSubview:labelC];
+
+    // Fonts
+    UILabel *labelF = [[UILabel alloc] init];
+    [labelF setFrame:CGRectMake(0, floorf(self.view.frame.size.height / 3), self.view.frame.size.width, floorf(self.view.frame.size.height / 3))];
+    [labelF setBackgroundColor:[UIColor clearColor]];
+    [labelF setTextAlignment:NSTextAlignmentCenter];
+    [labelF setText:@"This is a text with\nBOLD and ITALIC"];
+    [labelF setNumberOfLines:2];
+    [labelF setTextColor:[UIColor yellowColor]];
+    [labelF setFont:[UIFont fontWithName:@"Courier" size:14]];
+    [self.view addSubview:labelF];
+    
+
+    // Colors AND Fonts
+    UILabel *labelCF = [[UILabel alloc] init];
+    [labelCF setFrame:CGRectMake(0, floorf(self.view.frame.size.height * 2 / 3), self.view.frame.size.width, floorf(self.view.frame.size.height / 3))];
+    [labelCF setBackgroundColor:[UIColor clearColor]];
+    [labelCF setTextAlignment:NSTextAlignmentCenter];
+    [labelCF setText:@"This is a text with\nBOLD-RED and ITALIC-CYAN"];
+    [labelCF setNumberOfLines:2];
+    [labelCF setTextColor:[UIColor yellowColor]];
+    [labelCF setFont:[UIFont fontWithName:@"Courier" size:14]];
+    [self.view addSubview:labelCF];
+    
 }
 
 - (void)didReceiveMemoryWarning
